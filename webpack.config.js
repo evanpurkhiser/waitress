9781430e83a2f276599fb5efed4990faf66b8340
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-08-28 00:32:35 -0700

    Add favicons

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,23 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 const IS_PROD =
   process.argv.find(a => a.includes('mode=production')) !== undefined;
+
+const enabledFavicons = {
+  favicons: true,
+  android: false,
+  appleIcon: false,
+  appleStartup: false,
+  coast: false,
+  firefox: false,
+  opengraph: false,
+  twitter: false,
+  yandex: false,
+  windows: false,
+};
 
 module.exports = {
   entry: './app.js',
@@ -51,6 +65,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({ template: 'app.html' }),
     new SpriteLoaderPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './icons/folder.svg',
+      icons: enabledFavicons
+      prefix: 'favicons-[hash]/',
+    }),
     new webpack.HotModuleReplacementPlugin(),
   ],
 };
