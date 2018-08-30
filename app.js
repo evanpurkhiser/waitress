b23fa0db61644b8c3ead1b8a4357c9300df9c373
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-08-30 01:24:17 -0700

    Load fonts on page load

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const ravenKey = 'https://2afa25599321471fbc5dd9610bd74804@sentry.io/1256756';
 Raven.config(ravenKey, { environment }).install();
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Ubuntu:400');
-
   * {
     box-sizing: border-box;
   }
