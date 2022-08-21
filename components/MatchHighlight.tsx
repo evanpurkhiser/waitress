c3eac615e6b12bae4fbd010b865cf02655d17371
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-08-21 15:16:40 -0700

    Better highlight colors

--- components/MatchHighlight.tsx
+++ components/MatchHighlight.tsx
@@ -80,7 +80,8 @@ const MatchHighlight = ({match}: Props) => (
 );
 
 const Marker = styled('mark')`
-  background: #e7dbbd;
+  background: #ffb9d1;
+  box-shadow: 0px 1px 0 #f23978;
 `;
 
 export default MatchHighlight;
